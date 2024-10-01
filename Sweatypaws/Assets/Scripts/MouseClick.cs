7c8c6731c92b327cd151adfdb00fbf5675598725
TomiValtanen <tomi.valtanen1991@gmail.com>
2024-10-01 10:01:32 +0300

    [BOMB]
    Scripts added,
    mouseClick script that detect collider.

---
+++ Sweatypaws/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    void Update()
+    {
+        //If the left mouse button is clicked.
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Get the mouse position on the screen and send a raycast into the game world from that position.
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+            //If something was hit, the RaycastHit2D.collider will not be null.
+            if (hit.collider != null)
+            {
+              GetComponent<CheckAction>().action=hit.collider.gameObject;
+                
+            }
+        }
+    }
+}
