b4e2a64e7690510ac6fa1ef42a173e911737358b
TomiValtanen <tomi.valtanen1991@gmail.com>
2024-10-08 10:48:25 +0300

    [Fix]
    update

--- Sweatypaws/Assets/Scripts/MouseClick.cs
+++ Sweatypaws/Assets/Scripts/MouseClick.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MouseClick : MonoBehaviour
 {
@@ -12,7 +13,6 @@ public class MouseClick : MonoBehaviour
             //Get the mouse position on the screen and send a raycast into the game world from that position.
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
-
             //If something was hit, the RaycastHit2D.collider will not be null.
             if (hit.collider != null)
             {
