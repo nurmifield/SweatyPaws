157d37f0ae7bc925ddf1710fa78bdc4a6e3d324c
TomiValtanen <tomi.valtanen1991@gmail.com>
2024-10-08 11:29:22 +0300

    [Clikc]
    Update

--- Sweatypaws/Assets/Scripts/MouseClick.cs
+++ Sweatypaws/Assets/Scripts/MouseClick.cs
@@ -5,11 +5,22 @@ using UnityEngine.EventSystems;
 
 public class MouseClick : MonoBehaviour
 {
+    public GameObject menuPanel;
+
+    private void Start()
+    {
+        menuPanel.SetActive(false);
+    }
     void Update()
     {
         //If the left mouse button is clicked.
         if (Input.GetMouseButtonDown(0))
         {
+            if (menuPanel.activeSelf)
+            {
+                Debug.Log("Paneeli on aktiivinen");
+                return;
+            }
             //Get the mouse position on the screen and send a raycast into the game world from that position.
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
