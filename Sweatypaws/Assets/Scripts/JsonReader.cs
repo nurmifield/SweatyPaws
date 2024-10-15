ceb333ef5c17f0bb42ef3e8211e70db74c204c82
TomiValtanen <tomi.valtanen1991@gmail.com>
2024-10-15 11:00:00 +0300

    [Dialog]
    Dialogue tests

--- Sweatypaws/Assets/Scripts/JsonReader.cs
+++ Sweatypaws/Assets/Scripts/JsonReader.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class JsonReader : MonoBehaviour
 {
     public TextAsset jsonFile;
+    public TextAsset jsonPlayerFile;
     public DialogueList dialogueList = new DialogueList();
+    public Player player= new Player();
 
 
     [System.Serializable]
@@ -13,6 +15,7 @@ public class JsonReader : MonoBehaviour
     {
         public string character_name;
         public string dialog;
+        public string character_image;
     }
     [System.Serializable]
     public class DialogueList
@@ -20,12 +23,33 @@ public class JsonReader : MonoBehaviour
         public DialogueParts[] scene_1;
         public DialogueParts[] scene_2;
     }
+    [System.Serializable]
+    public class Player
+    {
+        public int level;
+        public DialogueProgress[] dialogue_progress;
+        public LevelProgress[] level_progress;
+    }
+    [System.Serializable]
+    public class DialogueProgress
+    {
+        public string dialogue_name;
+        public bool watched;
+    }
+    [System.Serializable]
+    public class LevelProgress
+    {
+        public string level_name;
+        public bool complete;
+        public int score;
+    }
 
-   
     // Start is called before the first frame update
     void Awake()
     {
-        dialogueList = JsonUtility.FromJson<DialogueList>(jsonFile.text); 
+        dialogueList = JsonUtility.FromJson<DialogueList>(jsonFile.text);
+        player=JsonUtility.FromJson<Player>(jsonPlayerFile.text);
+        
     }
 
 }
