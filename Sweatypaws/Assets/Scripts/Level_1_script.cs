b1cc361d0ba67b41d40192d4a7687ff1053cdd77
KilpiRon <127877603+KilpiRon@users.noreply.github.com>
2024-10-30 13:03:39 +0200

    Epäiltykansio
    Level 1 epäiltykansio

---
+++ Sweatypaws/Assets/Scripts/Level_1_script.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_1_script : MonoBehaviour
+{
+    public GameObject EpäiltykansioCanvas;
+    
+    public void OpenCanvas()
+    {
+        if (EpäiltykansioCanvas != null)
+        {
+            EpäiltykansioCanvas.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("EpäiltykansioCanvas is not assigned in the inspector.");
+        }
+    }
+}
