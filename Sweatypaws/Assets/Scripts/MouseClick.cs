48b31665dba864adb8aaa0b9eba20f1091912bb2
TomiValtanen <tomi.valtanen1991@gmail.com>
2024-11-07 11:03:00 +0200

    [MouseClick]
    Fixed

--- Sweatypaws/Assets/Scripts/MouseClick.cs
+++ Sweatypaws/Assets/Scripts/MouseClick.cs
@@ -8,21 +8,27 @@ using UnityEngine.UI;
 public class MouseClick : MonoBehaviour
 {
     public GameObject menuPanel;
-
+    GameObject obj;
     private void Start()
     {
         menuPanel.SetActive(false);
+        
     }
     void Update()
     {
         //If the left mouse button is clicked.
         if (Input.GetMouseButtonDown(0))
         {
-            if (menuPanel.activeSelf)
+            obj = GameObject.FindWithTag("MenuPanel");
+            if (obj != null)
             {
-                Debug.Log("Paneeli on aktiivinen");
-                return;
+                if (obj.activeSelf)
+                {
+                    Debug.Log("Paneeli on aktiivinen");
+                    return;
+                }
             }
+           
             //Get the mouse position on the screen and send a raycast into the game world from that position.
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
